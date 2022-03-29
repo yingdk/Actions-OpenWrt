c65a1833a03ce4c44bac59814c1b45a711bbd2f1
yingdk <59006685+yingdk@users.noreply.github.com>
2022-03-29 21:55:00 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -24,6 +24,9 @@
 #git clone https://github.com/sypopo/luci-theme-atmaterial.git package/luci-theme-atmaterial
 git clone https://github.com/Leo-Jo-My/luci-theme-opentomato.git package/luci-theme-opentomato
 
+#添加甜糖星愿插件
+git clone https://github.com/jerrykuku/luci-app-ttnode.git package/ttnode
+
 #添加ssrplus
 #git clone https://github.com/fw876/helloworld.git package/helloworld
 #添加passwall
